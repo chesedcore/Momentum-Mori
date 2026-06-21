4b4ae6f703b3aa6f762171be81a52b57ed81bfcb
Chesed <potatolegend200@gmail.com>
2026-06-21 15:02:45 +0530

    Revert "add the vector 1 datatype"
    This reverts commit ad9ad21f4dfcc0c06d57d951cc3f9d09156616ea.

--- Momentum Mori/scripts/blade/player.gd
+++ Momentum Mori/scripts/blade/player.gd
@@ -1,13 +1,15 @@
 class_name Player extends Blade
 
+##how fast the blade spins around the orbit center (radians/sec^2)
 @export var spin_speed: float = 4.0
+
+##radius of the orbit circle around the orbit center
 @export var orbit_radius: float = 60.0
-@export var move_acceleration: float = 2000.0
-@export var move_friction: float = 0.88
-@export var max_move_speed: float = 800.0
+
+@export var follow_weight: float = 3.0
+
 
 var _orbit_center: Vector2 = Vector2.ZERO
-var _move_velocity: Vector2 = Vector2.ZERO
 var _angle: float = 0.0
 
 func _ready() -> void {
@@ -15,21 +17,15 @@ func _ready() -> void {
 }
 
 func _physics_process(delta: float) -> void {
-	# WASD moves the orbit center
-	var input_dir := Input.get_vector(&"left", &"right", &"up", &"down")
-	_move_velocity += input_dir * move_acceleration * delta
-	_move_velocity *= pow(move_friction, delta * 60.0)
-	_move_velocity = _move_velocity.limit_length(max_move_speed)
-	_orbit_center += _move_velocity * delta
-
-	# Mouse rotates the blade around orbit center
 	var mouse_pos: Vector2 = get_global_mouse_position()
-	var to_mouse := mouse_pos - _orbit_center
-	_angle = to_mouse.angle()
+	
+	_orbit_center = _orbit_center.lerp(mouse_pos, follow_weight * delta)
+	
+	_angle = (global_position - _orbit_center).angle()
 	_angle += spin_speed * delta
-
+	
 	var target_pos := _orbit_center + Vector2.from_angle(_angle) * orbit_radius
-
+	
 	velocity = (target_pos - global_position) / delta
 	move_and_slide()
 }
--- Momentum Mori/scripts/utils/vec1.gd
+++
@@ -1,9 +0,0 @@
-class_name Vector1 extends RefCounted
-
-var x: float
-
-static func from(p_x: float) -> Vector1 {
-	var vec := new()
-	vec.x = p_x
-	return vec
-}
