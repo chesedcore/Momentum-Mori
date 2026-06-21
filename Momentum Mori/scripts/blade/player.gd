859fb2cf580946bcdb5559d1cd20678a056a68d8
Chesed <potatolegend200@gmail.com>
2026-06-21 16:09:22 +0530

    BEST CONTROL SCHEME EVER

--- Momentum Mori/scripts/blade/player.gd
+++ Momentum Mori/scripts/blade/player.gd
@@ -2,14 +2,13 @@ class_name Player extends Blade
 
 ##how fast the blade spins around the orbit center (radians/sec^2)
 @export var spin_speed: float = 4.0
-
 ##radius of the orbit circle around the orbit center
 @export var orbit_radius: float = 60.0
-
-@export var follow_weight: float = 3.0
-
+@export var max_follow_speed: float = 5000.0
+@export var frictional_damp_delta_inverse: float = 0.92
 
 var _orbit_center: Vector2 = Vector2.ZERO
+var _orbit_velocity: Vector2 = Vector2.ZERO
 var _angle: float = 0.0
 
 func _ready() -> void {
@@ -19,7 +18,16 @@ func _ready() -> void {
 func _physics_process(delta: float) -> void {
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	
-	_orbit_center = _orbit_center.lerp(mouse_pos, follow_weight * delta)
+	var to_mouse := mouse_pos - _orbit_center
+	var dist := to_mouse.length()
+	
+	if dist > 2.0:
+		var force := to_mouse.normalized() * pow(dist, 1.5) * 2.0
+		_orbit_velocity += force * delta
+	
+	_orbit_velocity *= pow(frictional_damp_delta_inverse, delta * 60.0)
+	_orbit_velocity = _orbit_velocity.limit_length(max_follow_speed)
+	_orbit_center += _orbit_velocity * delta
 	
 	_angle = (global_position - _orbit_center).angle()
 	_angle += spin_speed * delta
