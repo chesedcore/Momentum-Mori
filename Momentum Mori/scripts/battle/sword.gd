6c294d13f2f52b9b36c0b3a9e94caab5b456b1a8
Gael <farhanalnoor43@gmail.com>
2026-06-25 02:03:13 +0500

    fuck it homing swords

--- Momentum Mori/scripts/battle/sword.gd
+++ Momentum Mori/scripts/battle/sword.gd
@@ -1,8 +1,8 @@
 class_name Sword extends Projectile
 
 
-
-
+var homing_time : float = 3
+var turn_speed: float = 3.0 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void{
 	queue_free.call_deferred()
@@ -14,3 +14,20 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 		body.take_damage(5)
 		EventBus.spawn_spark.emit(body)
 		queue_free.call_deferred()
+
+
+
+func _physics_process(delta: float) -> void {
+	if homing_time > 0 {
+		homing_time -= delta
+		var target_angle = global_position.direction_to(target.global_position).angle()
+		var current_angle = fire_dir.angle()
+		var new_angle = lerp_angle(current_angle, target_angle, turn_speed * delta)
+		fire_dir = Vector2.from_angle(new_angle)
+		rotation = new_angle
+	} 
+	if is_firing {
+		velocity = fire_dir * speed
+		move_and_slide()
+	}
+}
