82d58c807196cb63ea60435a8bb2b88e0e4c0820
Gael <farhanalnoor43@gmail.com>
2026-06-26 17:08:41 +0500

    fixed incline bug for all blades also made angry enemy omea mo shinderu you

--- Momentum Mori/scripts/blade/angry_boss.gd
+++ Momentum Mori/scripts/blade/angry_boss.gd
@@ -1,1 +1,55 @@
 class_name AngryBoss extends EnemyBlade
+
+var teleport_cooldown : float = 4
+var remaining_teleport_cooldown : float = 0
+
+
+@export var teleport_threshold: float = 0.75
+
+@export var teleport_distance: float = 400
+
+
+
+@export var teleport_min_distance: float = 300  
+@export var teleport_max_distance: float = 800
+
+
+func  _ready() -> void{
+	super._ready()
+	
+}
+
+func _physics_process(delta: float) -> void{
+	
+	if remaining_teleport_cooldown >= teleport_cooldown{
+		# check the targets velocity to see if its heading towards this blade and teleport this blade behhind  the target 
+		var target_velocity = target.velocity
+		if target_velocity.length() > 0{
+			var target_move_dir = target_velocity.normalized()
+			var to_self = target.global_position.direction_to(global_position)
+			var dot = target_move_dir.dot(to_self)
+			var dist = global_position.distance_to(target.global_position)
+			if dot >= teleport_threshold and dist >= teleport_min_distance and dist <= teleport_max_distance{
+				teleport_behind_target()
+				remaining_teleport_cooldown = 0
+			}
+		}
+	}
+	remaining_teleport_cooldown += delta
+	
+	super._physics_process(delta)
+
+}
+@onready var circle: Circle = $Incline/Visual/Visual/Control/Circle
+
+func teleport_behind_target() -> void{
+	var flash_tween = create_tween()
+	flash_tween.tween_property(circle,"modulate",Color.WHITE,0.05)
+	await  flash_tween.finished
+	var target_move_dir = target.velocity.normalized()
+	global_position = target.global_position - target_move_dir * teleport_distance
+	var to_target = global_position.direction_to(target.global_position)
+	change_to_attack(to_target)
+	var unflash_tween = create_tween()
+	unflash_tween.tween_property(circle,"modulate",Color.RED,0.05)
+}
---
+++ Momentum Mori/scripts/blade/bladevisual.gd
@@ -0,0 +1,1 @@
+extends CascadeV3
