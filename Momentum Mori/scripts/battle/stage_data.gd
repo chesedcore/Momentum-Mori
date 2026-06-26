9fcc16602afa2aef69c346929936e5f1be91a7bd
Chesed <potatolegend200@gmail.com>
2026-06-27 03:55:58 +0530

    stage additions + menu music

---
+++ Momentum Mori/scripts/battle/stage_data.gd
@@ -0,0 +1,60 @@
+class_name StageData extends Resource
+
+enum Track {
+	RANDOM,
+	HEAVENS_BELOW,
+	SANGUINE_SPIN,
+	SCION_OF_DARKNESS,
+	PURGATORY,
+}
+
+@export var intro_lines: Array[String] = [
+	"monarch forgot to add text to this bullshit"
+]
+
+@export var track_to_play: Track
+
+@export var waves: Array[WaveData] 
+@export var boss: BossCharacter.Boss
+
+func get_current_track_stream() -> Option[AudioStream] {
+	var sound: String
+	match track_to_play:
+		Track.HEAVENS_BELOW:
+			sound = "res://assets/audio/music/heavens_below.ogg"
+		Track.SANGUINE_SPIN:
+			sound = "res://assets/audio/music/sanguine_spin.ogg"
+		Track.SCION_OF_DARKNESS:
+			sound = "res://assets/audio/music/scion_of_darkness.ogg"
+		Track.PURGATORY:
+			sound = "res://assets/audio/music/purgatory.ogg"
+		Track.RANDOM:
+			sound = [
+				"res://assets/audio/music/heavens_below.ogg",
+				"res://assets/audio/music/sanguine_spin.ogg",
+				"res://assets/audio/music/scion_of_darkness.ogg",
+				"res://assets/audio/music/purgatory.ogg"
+			].pick_random() as String
+		_: 
+			assert(false, "minions don't get vaccinations \nwhy should your children")
+	
+	if sound: return Option.some(load(sound))
+	return Option.none()
+}
+
+func map_wave_to_blades(idx: int) -> Option[Array[Blade]] {
+	if idx < waves.size() - 1 {
+		return Option.none()
+	}
+	
+	var target_wave := waves[idx]
+	var blades: Array[Blade]
+	
+	for blade_type in target_wave.enemy_map {
+		var number_of_times_that_blade_appears := target_wave.enemy_map[blade_type]
+		for i in number_of_times_that_blade_appears {
+			blades.push_back(Registry.blade_from_type(blade_type))
+		}
+	}
+	return Option.some(blades)
+}
--- Momentum Mori/scripts/utils/registry.gd
+++ Momentum Mori/scripts/utils/registry.gd
@@ -1,9 +1,35 @@
 class_name Registry
 
+const BladeType = WaveData.BladeType
+
 static func create_chain_whip() -> ChainWhip {
 	return preload("res://scenes/battle/repeating_chain.tscn").instantiate()
 }
 
 static func create_options() -> Options {
 	return load("res://scenes/ui/options_pane.tscn").instantiate()
+}
+
+static func blade_from_type(type: BladeType) -> Blade {
+	var scene: String
+	match type:
+		BladeType.ANGRY_GUY:
+			scene = "res://scenes/blade/angry_boss.tscn"
+		BladeType.AOE_GUY:
+			scene = "res://scenes/blade/aoe_boss.tscn"
+		BladeType.CLONE_GUY:
+			scene = "res://scenes/blade/clone_boss.tscn"
+		BladeType.DRACULA_HIMSELF:
+			scene = "res://scenes/blade/dracula.tscn"
+		BladeType.SOME_GUY:
+			scene = "res://scenes/blade/enemy_blade.tscn"
+		BladeType.PROJECTILE_GUY:
+			scene = "res://scenes/blade/spinning_projectile_enemy.tscn"
+		BladeType.SWORD_GUY:
+			scene = "res://scenes/blade/sword_boss.tscn"
+		_:
+			assert(false, "go kill monarch")
+			return null
+	if scene: return load(scene).instantiate()
+	return null
 }
