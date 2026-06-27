55425a81004b7dfed0ec9727c88e4032f6eab23e
Chesed <potatolegend200@gmail.com>
2026-06-27 21:18:52 +0530

    time to wire the actual gameplay

--- Momentum Mori/scripts/battle/stage_handler.gd
+++ Momentum Mori/scripts/battle/stage_handler.gd
@@ -6,19 +6,22 @@ class_name StageHandler extends Control
 var data: StageData
 
 static func from(stage_data: StageData) -> StageHandler {
-	var handler := new()
+	var handler := Registry.create_stage_handler()
 	handler.data = stage_data
 	return handler
 }
 
 func _ready() -> void {
-	print(get_children())
-	await get_tree().process_frame
-	var intro := _initiate_intro_sequence()
+	_initiate_intro_sequence()
 }
 
 func _initiate_intro_sequence() -> IntroSequence {
 	var intro := IntroSequence.from_lines(data.get_lines())
 	intro_dock.add_child(intro)
+	intro._internal_finished.connect(start_game, CONNECT_ONE_SHOT)
 	return intro
+}
+
+func start_game() -> void {
+	
 }
